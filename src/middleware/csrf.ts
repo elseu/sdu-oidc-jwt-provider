e476d9319f91fd9ded4ad0c814248b481dd7a187
Tim de Wolf <tpdewolf@gmail.com>
2024-05-15 16:50:34 +0200

    feat: Check if access token if valid if isnt csrf (#29)

--- src/middleware/csrf.ts
+++ src/middleware/csrf.ts
@@ -1,18 +1,66 @@
 import * as Koa from "koa";
 import { AppSessionState } from "./app-session";
+import { verify } from "../util/jwt-promise";
+import { JwtHeader, SigningKeyCallback } from "jsonwebtoken";
+import * as jose from "node-jose";
 
-export function csrfTokenAuth(): Koa.Middleware<AppSessionState> {
+function getTokenFromContext(ctx: Koa.DefaultContext): string | null {
+  const authHeader = ctx.headers.authorization;
+  let token: null | string = null;
+  if (authHeader) {
+    const match = authHeader.match(/^Bearer[ ]+(.*)$/);
+    if (match) {
+      token = match[1];
+    }
+  }
+  return token;
+}
+
+interface CsrfTokenAuthOptions {
+  keystore: jose.JWK.KeyStore;
+}
+
+export function csrfOrAccessTokenAuth(
+  options?: CsrfTokenAuthOptions
+): Koa.Middleware<AppSessionState> {
   return async (ctx, next) => {
     if (
-      ctx.get("Authorization") !== `Bearer ${ctx.state.appSession.csrfToken}`
+      ctx.get("Authorization") === `Bearer ${ctx.state.appSession.csrfToken}`
     ) {
-      ctx.status = 403;
-      ctx.body = {
-        status: "error",
-        message: "Invalid bearer token",
-      };
+      await next();
       return;
     }
-    await next();
+
+    const token = getTokenFromContext(ctx);
+
+    if (token && options?.keystore) {
+      const result = await verify(
+        token,
+        async (header: JwtHeader, callback: SigningKeyCallback) => {
+          if (!header.kid) {
+            callback("No kid in JWT header");
+            return;
+          }
+          const key = options.keystore.get(header.kid);
+          if (!key) {
+            callback("Unknown kid");
+            return;
+          }
+          callback(null, (await jose.JWK.asKey(key)).toPEM());
+        }
+      );
+
+      // token is valid. continue
+      if (result?.s) {
+        await next();
+        return;
+      }
+    }
+
+    ctx.status = 403;
+    ctx.body = {
+      status: "error",
+      message: "Invalid bearer token",
+    };
   };
 }

ba7faaceb109d8affd76399620ea20e8199ff3a9
Tim de Wolf <tpdewolf@gmail.com>
2024-05-15 09:00:35 +0200

    Update GHA

--- src/middleware/csrf.ts
+++ src/middleware/csrf.ts
@@ -2,18 +2,17 @@ import * as Koa from "koa";
 import { AppSessionState } from "./app-session";
 
 export function csrfTokenAuth(): Koa.Middleware<AppSessionState> {
-    return async (ctx, next) => {
-        if (
-            ctx.get("Authorization") !==
-            `Bearer ${ctx.state.appSession.csrfToken}`
-        ) {
-            ctx.status = 403;
-            ctx.body = {
-                status: "error",
-                message: "Invalid bearer token",
-            };
-            return;
-        }
-        await next();
-    };
+  return async (ctx, next) => {
+    if (
+      ctx.get("Authorization") !== `Bearer ${ctx.state.appSession.csrfToken}`
+    ) {
+      ctx.status = 403;
+      ctx.body = {
+        status: "error",
+        message: "Invalid bearer token",
+      };
+      return;
+    }
+    await next();
+  };
 }
